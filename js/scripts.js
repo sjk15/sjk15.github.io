71535843fcbb2cdb7e690129ceab99e0d289b7e7
Oscar Björkman <git@oscarb.se>
2016-04-17 14:36:16 +0200

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+// Execute the function markWeek when document is ready
+// Dependencies: jQuery
+$(markWeek);
+
+function markWeek(jQuery) {
+  var date = new Date();
+  var currentWeek = date.getWeek();
+
+  var startWeek2016 = $("#2016 + table tbody td:first").text(); // 2
+  var currentWeekTableRow = currentWeek - startWeek2016;
+
+  // Style weeks in the past in 2015
+  $("#2015 + table tbody tr").toggleClass("past-week");
+
+  // Style weeks in the past in 2016
+  $("#2016 + table tbody tr:lt(" + currentWeekTableRow + ")").toggleClass("past-week");
+
+  // Style current week in 2016
+  $("#2016 + table tbody tr:eq(" + currentWeekTableRow + ")").toggleClass("active-week");
+
+}
+
+
+
+// Source: http://weeknumber.net/how-to/javascript
+// Returns the ISO week of the date.
+Date.prototype.getWeek = function() {
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+}
