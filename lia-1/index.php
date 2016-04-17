557c185e48ab444ddea8a26b1ca1d4de6173d5ee
Oscar Björkman <git@oscarb.se>
2016-04-17 14:46:23 +0200

    Add resources for LiA

---
+++ lia-1/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Lärande i Arbete - del 1</title>
+<?php 
+include_once "../includes/header.php";
+?>
+</head>
+<body><div class="container"><h1 id="lärande-i-arbete-del-1">Lärande i Arbete - del 1 </h1>
+
+<p><div class="toc">
+<ul>
+<li><a href="#lärande-i-arbete-del-1">Lärande i Arbete - del 1 </a><ul>
+<li><a href="#info">Info</a></li>
+<li><a href="#resurser">Resurser</a></li>
+<li><a href="#examination">Examination</a></li>
+<li><a href="#lektionstillfällen">Lektionstillfällen</a></li>
+<li><a href="#översikt">Översikt</a><ul>
+<li><a href="#vecka-1122-2016">Vecka 11–22, 2016</a></li>
+</ul>
+</li>
+</ul>
+</li>
+</ul>
+</div>
+</p>
+
+<h2 id="info">Info</h2>
+
+<ul>
+<li><a href="kursplan.pdf">Kursplan</a> (pdf)</li>
+</ul>
+
+<p>Frågor om LiA hänvisas till utbildningsledaren.</p>
+
+
+
+<h2 id="resurser">Resurser</h2>
+
+<ul>
+<li><a href="intyg.pdf">Intyg/betyg över fullgjord LIA-period</a> (pdf)</li>
+<li><a href="utvecklingsplan.pdf">Utvecklingsplan för Lärande i arbete (LIA)</a> (pdf)</li>
+<li><a href="foretagslista.pdf">Företagslista</a> (pdf)</li>
+<li><a href="handlingsplan.pdf">Handlingsplan för sökande av LIA</a> (pdf)</li>
+<li><a href="information-till-foretag.pdf">LIA information till företag</a> (pdf)</li>
+<li><a href="telefon.pdf">Söka LIA per telefon</a> (pdf)</li>
+</ul>
+
+
+
+<h2 id="examination">Examination</h2>
+
+<p>Handledarens rapport.</p>
+
+
+
+<h2 id="lektionstillfällen">Lektionstillfällen</h2>
+
+<p>Inga lektionstillfällen ges under LiA-perioden.</p>
+
+
+
+<h2 id="översikt">Översikt</h2>
+
+
+
+<h3 id="vecka-1122-2016">Vecka 11–22, 2016</h3></div></body>
+</html>
