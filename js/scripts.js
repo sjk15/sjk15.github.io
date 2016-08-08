f271c3e456fba49f8400ecac2f89d8a59b68ebba
mfris <martin.frisk9103@gmail.com>
2016-08-08 15:44:20 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ function markWeek(jQuery) {
   var currentWeek = date.getWeek();
 
   var startWeek2016 = $("h3:contains(2016) + table tbody td:first").text(); // 2
-  var currentWeekTableRow = currentWeek- startWeek2016;
+  var currentWeekTableRow = currentWeek- startWeek2016 -10;
 
   // Style weeks in the past in 2015
   $("h3:contains(2015) + table tbody tr").toggleClass("past-week");
